bef5c2428abedf77691a81399ee0c6a39574d1a1
Rex Nowacki <nowacki.chris@gmail.com>
2023-09-11 13:57:43 -0700

    added 1/2 of module 3 project

---
+++ clang/projects/module3/integer_to_octal.c
@@ -0,0 +1,34 @@
+// Christopher Nowacki || Cis 265
+// Integer to Octal
+
+#include <stdio.h>
+
+int main() {
+  int user_input;
+  int oct_digit1, oct_digit2, oct_digit3, oct_digit4, oct_digit5;
+
+  printf("Enter a number between 0 and 32767: ");
+  scanf("%d", &user_input);
+
+  // Check if the number is within the specified range
+  if (user_input < 0 || user_input > 32767) {
+    printf("Number out of range.\n");
+    return 1;
+  }
+
+  // Calculate the 5 octal digits from right to left
+  // 1. Divide input by 8; remainder is 5th digit
+  // 2. Divide input by 8 twice, remainder is 4th digit.
+  // 3. Divide input by 8 thrice, remainder is 3rd digit.
+  // 4. Divide input by 8 four times, remainder is 2nd digit.
+  // 5. Divide input by 8 five times, remainder is 1st digit.
+  oct_digit1 = user_input % 8;
+  oct_digit2 = (user_input / 8) % 8;
+  oct_digit3 = (user_input / 8 / 8) % 8;
+  oct_digit4 = (user_input / 8 / 8 / 8) % 8;
+  oct_digit5 = (user_input / 8 / 8 / 8 / 8) % 8;
+
+  printf("In octal, your number is:%d%d%d%d%d\n", oct_digit5, oct_digit4, oct_digit3, oct_digit2, oct_digit1);
+
+  return 0;
+}
