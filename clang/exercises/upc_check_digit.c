12accfb8e5f88d9debcd1934f1d94cc4bb80beb0
Rex Nowacki <nowacki.chris@gmail.com>
2023-09-11 22:45:37 -0700

    updated upc algo

--- clang/exercises/upc_check_digit.c
+++ clang/exercises/upc_check_digit.c
@@ -12,9 +12,9 @@ int main(){
   scanf("%1d%1d%1d%1d%1d", &j1, &j2, &j3, &j4, &j5);
   
   // Determine check digit
-  first_sum = d + i2 + i4 +j1 +j3 +j5;
-  sec_sum = i1 + i3 + i5 + j2 +j4;
-  total = ((3 * first_sum) + sec_sum - 1) % 10;
-  check_digit = 9 - total;
+  first_sum = d + i2 + i4 + j1 + j3 + j5;
+  sec_sum = i1 + i3 + i5 + j2 + j4;
+  check_digit = 9 - (((3 * first_sum) + sec_sum - 1) % 10);
+  // check_digit = 9 - total;
   printf("The check digit is: %d\n", check_digit);
 }
