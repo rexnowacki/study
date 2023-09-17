d01bb9186969aed556e5d88568ba7c8aadb98909
Rex Nowacki <nowacki.chris@gmail.com>
2023-09-17 11:48:13 -0700

    w/e

--- cpplang/projects/smallest_integer.cpp
+++ cpplang/projects/smallest_integer.cpp
@@ -5,27 +5,27 @@ int main() {
     int n, num;
     int smallest;
 
-    std::cout << "Enter the number of integers to compare: ";
-    std::cin >> n;
+   cout << "Enter the number of integers to compare: ";
+   cin >> n;
 
     if (n <= 0) {
-        std::cout << "You must enter a positive number for the number of integers to compare." << std::endl;
+       cout << "You must enter a positive number for the number of integers to compare." <<endl;
         return 1;
     }
 
-    std::cout << "Enter integer 1: ";
-    std::cin >> smallest; // Initialize 'smallest' with the first number entered
+   cout << "Enter integer 1: ";
+   cin >> smallest; // Initialize 'smallest' with the first number entered
 
     for (int i = 1; i < n; ++i) { // Start loop from 1 because we already have the first integer
-        std::cout << "Enter integer " << (i + 1) << ": ";
-        std::cin >> num;
+       cout << "Enter integer " << (i + 1) << ": ";
+       cin >> num;
 
         if (num < smallest) {
             smallest = num;
         }
     }
 
-    std::cout << "The smallest integer is: " << smallest << std::endl;
+   cout << "The smallest integer is: " << smallest <<endl;
 
     return 0;
 }
