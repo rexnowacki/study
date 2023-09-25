8fa8b597b303e222c9c17f500ebccf105b77bb5c
Rex Nowacki <nowacki.chris@gmail.com>
2023-09-24 21:46:38 -0700

    okayyy

--- clang/projects/module4/find_highest_from_series.c
+++ clang/projects/module4/find_highest_from_series.c
@@ -1,22 +1,31 @@
 // Christopher Nowacki
 // CIS 265
-// Largest Number from a Series
+// Largest Number from a Series -- Module 4
 
 #include <stdio.h>
 
 int main() {
   double number, largest;
-
+  int validate;
   // Prompt user for input
   printf("Enter a number (enter a number equal to or less than 0 to quit): ");
-  scanf("%lf", &number);
-
+  validate = scanf("%lf", &number);
+  // Clears buffer to avoid infinite loop if input ! double
+  while(getchar() != '\n');  
+  if (validate != 1) {
+    printf("Non-digits entered. Try again.\n");
+  }
+  
   // If the first number itself is negative or 0, ask again
   while (number <=0) {
     printf("Please enter a non-negative number to start with: ");
-    scanf("%lf", &number);
+    validate = scanf("%lf", &number);
+    while(getchar() != '\n');
+    if (validate != 1) {
+      printf("Non-digits entered. Try again.\n");
+    }
   }
-
+  
   largest = number; // Initialize largest with the first valid non-negative number
 
   // Loop to get numbers until 0 or a negative number is entered
@@ -28,10 +37,19 @@ int main() {
     }
     // Prompt again
     printf("Enter another number (enter a number equal to or less than 0 to quit): ");
-    scanf("%lf", &number);
+    validate = scanf("%lf", &number);
+    while(getchar() != '\n');
+    if (validate != 1) {
+      printf("Non-digits entered. Try again.\n");
+    }
   }
 
   // Display results
+  if (largest > 0) {
   printf("The largest non-negative number entered was: %.2lf\n", largest);
   return 0;
+  }
+  else {
+    return 0;
+  }
 }
--- clang/projects/module4/one_month_calendar.c
+++ clang/projects/module4/one_month_calendar.c
@@ -1,58 +1,29 @@
 // Christopher Nowacki
 // CIS 265
-// Project Name
-
-// Write a program that prints a one-month calnedar. The user specifies
-// the number of days in the month and the day of the week on which the
-// month begins:
-//      Enter number of days in the month: 31
-//      Enter starting day of the week (1=Sun, 7=Sat): 3
-//
-//             1  2  3  4  5
-//       6  7  8  9 10 11 12
-//      13 14 15 16 17 18 19
-//      20 21 22 23 24 25 26
-//      27 28 29 30 31
-
-// This Needs Work ** 
-
+// Calendar Program -- Module 4
 #include <stdio.h>
 
-int main() {
-  int days, start_day;
-  int friday_13th;
-  printf("Enter number of days in the month: ");
-  scanf("%d", &days);
-
-  printf("Enter starting day of the week (1=Sun, 2=Mon, 3=Tue, 4=Wed, 5=Thu, 6=Fri, 7=Sat): ");
-  scanf("%d", &start_day);
-
-  // Print the header for the calendar
-  printf("\n");
-
-    // Print leading spaces for the first week
-  for (int i = 0; i < start_day; i++) {
-    printf("    ");
-  }
-
-  // Print the days
-  for (int day = 1; day <= days; day++) {
-    printf("%3d ", day);
-
-    // Detect Jason Voorhes
-    if(day == 13 && (day + start_day) % 7 == 6) {
-      friday_13th = 1;
-    } 
-
-    // If the day is a SAturday or the last day of the month, print newline
-    if ((day + start_day) % 7 == 0 || day == days) {
-      printf("\n");
+int main(void) {
+    // Declare integers total_days, day, weekday, position
+    int total_days, day, weekday, position;
+    // Get number of days in month, assign to total_days
+    printf("Enter number of days in month: ");
+    scanf("%d", &total_days);
+    // Get starting day of the week, assign to weekday
+    printf("Enter starting day of the week (1=Mon, 7=Sun): ");
+    scanf("%d", &weekday);
+    // Print Header
+    printf("\n Mo Tu We Th Fr Sa Su\n");
+    // Loop for printing calendar to terminal
+    for (position = 1, day = 1; position <= total_days + weekday - 1; position++) {
+        if (position < weekday)
+            printf("   ");
+        else
+            printf("%3d", day++);
+        if (position % 7 == 0)
+            printf("\n");
     }
-  }
-  // Warn user of Spooky Szn
-  if (friday_13th) {
-    printf("Beware! Friday falls on the 13th this month.");
-  }
 
-  return 0;
+    printf("\n");
+    return 0;
 }
