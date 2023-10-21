4ec1bb1ec92e08280016b6c8e0ab37db6fd64513
Rex Nowacki <nowacki.chris@gmail.com>
2023-10-20 22:24:02 -0700

    woop there it isn't

--- cpplang/projects/module9/small_array.cpp
+++ cpplang/projects/module9/small_array.cpp
@@ -2,14 +2,14 @@
 #include <iostream>
 using namespace std;
 
-int recursiveMinimum(const int arr[], int start, int end) {
+int recursive_minimum(const int arr[], int start, int end) {
     // Base case: If start equals end, return the element at that position
     if (start == end) {
         return arr[start];
     }
 
     // Recursive case: Find the minimum of the current element and the minimum of the rest of the array
-    int minOfRest = recursiveMinimum(arr, start + 1, end);
+    int minOfRest = recursive_minimum(arr, start + 1, end);
     if (arr[start] < minOfRest) {
         return arr[start];
     } else {
@@ -18,8 +18,11 @@ int recursiveMinimum(const int arr[], int start, int end) {
 }
 
 int main() {
+    int start, end;
     int arr[] = {3, 13, 4, 10, 5, 9, 2, 6, 5};
     int size = sizeof(arr) / sizeof(arr[0]);
-    cout << "The minimum element in the array is: " << recursiveMinimum(arr, 0, size - 1) << endl;
+    start = 0;
+    end = size - 1;
+    cout << "The minimum element in the array is: " << recursive_minimum(arr, start, end) << endl;
     return 0;
 }
